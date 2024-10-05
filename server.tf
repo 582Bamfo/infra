96c3b85b7268095991924755101f582d9ed4b45f
Nana Aba <nabdennis@gmail.com>
2024-10-05 12:58:51 -0400

    increased the instance type

--- server.tf
+++ server.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "infra" {
   ami           = "ami-0fed63ea358539e44"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   metadata_options {
     http_tokens = "required"
   }
