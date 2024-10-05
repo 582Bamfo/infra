98af0e240415af3ca6073e0bf5c6bfc009c768aa
dbamfo <bamfodaniel@yahoo.com>
2024-10-05 13:58:09 +0100

    intitial code structure

--- server.tf
+++ server.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "infra" {
   ami           = "ami-0fed63ea358539e44"
   instance_type = "t2.small"
   metadata_options {
-    http_tokens = "enabled"
+    http_tokens = "required"
   }
 }
